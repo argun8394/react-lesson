7d07630dfc87319f169837ea0c5b5c60c3e4190f
argun8394 <argundoganali@gmail.com>
2021-09-13 21:54:50 +0300

    mouseEvent

--- functionalComponent_09_08/functcomp/src/App.js
+++ functionalComponent_09_08/functcomp/src/App.js
@@ -1,5 +1,4 @@
-import EventBind from './components/eventBinding/EventBind'
-// import MouseEvents from './components/mouseEvents/MouseEvents'
+import MouseEvents from './components/mouseEvents/MouseEvents'
 // import KeyboardEvents from './components/keyboardEvents/KeyboardEvents'
 // import Form from './components/form/Form'
 import './App.css'
@@ -8,8 +7,7 @@ const App = () => {
     return (
         <div className="App">
             {/* <h1>Events in React</h1> */}
-            <EventBind />
-            {/* <MouseEvents /> */}
+            <MouseEvents />
             {/* <KeyboardEvents /> */}
             {/* <Form /> */}
         </div>
--- functionalComponent_09_08/functcomp/src/components/mouseEvents/MouseEvents.js
+++ functionalComponent_09_08/functcomp/src/components/mouseEvents/MouseEvents.js
@@ -1,7 +1,11 @@
 import "./styles.css";
 
+import {useState} from "react";
+
 const MouseEvents = () => {
 
+  const [coordX, setCoordX]= useState("");
+  const [coordY, setCoordY]= useState("");
     /**
    * TODO list
    * click
@@ -9,15 +13,18 @@ const MouseEvents = () => {
    * mouseMove
    */
   // click event
+  const handleMouseMove =(e)=>{
+setCoordX(e.nativeEvent.offsetX);
+setCoordY(e.nativeEvent.offsetY);  }
 
   return (
     <>
       <h2>MouseEvents</h2>
       <p><span>X</span> and Y</p>
-      <p><span>CoordX here</span> CoordY here </p>
+      <p><span>{coordX}</span> {coordY} </p>
       <div style={{ display: "flex", justifyContent: "center" }}>
       <ul>
-        <li id="todo-1" >
+        <li id="todo-1" onMouseMove={handleMouseMove}>
           todo item 1 <span>X</span>
         </li>
         <li id="todo-2 " >
