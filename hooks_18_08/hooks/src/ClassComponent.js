8d3dc71c0fc29f6dd542fd133621151ca0949680
argun8394 <argundoganali@gmail.com>
2021-10-05 00:27:30 +0300

    class comp

--- hooks_18_08/hooks/src/ClassComponent.js
+++ hooks_18_08/hooks/src/ClassComponent.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
 class ClassComponent extends React.Component {
+    constructor(){
+        super();
+        this.state={
+            counter:0
+        };
+        this.increase=this.increase.bind(this);
+    }
+    increase(){
+        this.setState({counter:this.state.counter +1});
+    }
+    
     render() {
         return (
             <div className="class">
                 <h2>Class Component</h2>
+                <p>Counter:{ this.state.counter }</p>
+                <button onClick={() => this.increase()}>Increase</button>
             </div>
         );
     }
